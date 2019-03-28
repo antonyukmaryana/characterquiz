11637484db86d80c3c318b6f46ff95d0de3bc1a4
Kevin Garvey and Maryana Antonyuk <maryana@>
2019-03-28 16:45:30 -0700

    Add basic html, js, and css

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+dark =
+lawful =
+brave =
+
+var brave = parseInt($("select#brave1").val()) + parseInt($("select#brave1").val()) + parseInt($("select#brave1").val());
+var light = parseInt($("select#light1").val()) + parseInt($("select#light1").val()) + parseInt($("select#light1").val());
+var law = parseInt($("select#law1").val()) + parseInt($("select#law1").val()) + parseInt($("select#law1").val());
+
+
+if light > 0
+  if law >= 0
+    if brave >= 0
+
+else if light < 0
+  if law >= 0
+    if brave >0
+
+else light = 0
